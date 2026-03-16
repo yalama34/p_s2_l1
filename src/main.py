e7ce8c848b05f4a3d09c773b7c16329af2409413
Ярослав Прудников <yaprudnikov@mai.education>
2026-03-16 12:07:57 +0300

    Output is now readable

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ from .task_loader import TaskLoader
 
 def main():
     api_source: APISource = APISource(url="http://example.com")
-    file_source: FileSource = FileSource(path="/p_s2_l1/tests/example.txt")
+    file_source: FileSource = FileSource(path="C:/labs/python/sem2/p_s2_l1/example.txt")
     generator_source: GeneratorSource = GeneratorSource(seed=13, count=5)
     loader: TaskLoader = TaskLoader()
 
@@ -11,7 +11,9 @@ def main():
     loader.add_source(file_source)
     loader.add_source(generator_source)
 
-    print(loader.get_tasks())
+    tasks = loader.get_tasks()
+    for task in tasks:
+        print(task)
 
 
 if __name__ == "__main__":
