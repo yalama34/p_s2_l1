3869c3f70d5e04f4c66a2ec0bb8ec9bcfd11d354
Ярослав Прудников <yaprudnikov@mai.education>
2026-04-26 16:37:01 +0300

    fix: fix event loop blocking in RandomHandler

--- src/handlers/priority_handler.py
+++ src/handlers/priority_handler.py
@@ -7,9 +7,9 @@ logger = logging.getLogger(__name__)
 
 class PriorityHandler:
     async def handle(self, task: Task) -> None:
-        task.change_status()
+        await asyncio.to_thread(task.change_status)
         await asyncio.sleep(task.priority * 0.1)
         logger.info(f"Task {task.id} handled successfully.")
-        task.change_status()
+        await asyncio.to_thread(task.change_status)
 
 
--- src/handlers/random_handler.py
+++ src/handlers/random_handler.py
@@ -1,5 +1,6 @@
 import logging
 import random
+import asyncio
 
 from ..engine.task import Task
 from ..engine.task_errors import TaskError
@@ -14,11 +15,13 @@ class RandomHandler:
         random.seed(self._seed)
 
     async def handle(self, task: Task) -> None:
-        task.change_status()
-        if random.random() < 0.2:
+        await asyncio.to_thread(task.change_status)
+        chance = await asyncio.to_thread(random.random)
+        logger.info(f"Task {task.id} handled successfully.")
+        if chance < 0.2:
             logger.warning(f"Simulating failure for task {task.id}")
-            task.failure()
+            await asyncio.to_thread(task.failure)
             raise TaskError(f"Task {task.id} failed")
         else:
             logger.info(f"Task {task.id} handled successfully.")
-            task.change_status()
+            await asyncio.to_thread(task.change_status)
